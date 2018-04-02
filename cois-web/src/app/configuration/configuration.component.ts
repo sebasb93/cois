1cf091621db6476bfe8af55dbc971bc57a9f8094
Sebastian Velasquez Quintero <sebasb93@gmail.com>
2018-04-02 17:27:45 -0500

    #8: add components structure

---
+++ cois-web/src/app/configuration/configuration.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'configuration-component',
+  templateUrl: './configuration.component.html'  
+})
+
+export class ConfigurationComponent {
+  
+}
---
+++ cois-web/src/app/inventary/inventary.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'inventary-component',
+  templateUrl: './inventary.component.html'  
+})
+
+export class InventaryComponent {
+  
+}
