11b1a21960258011925cdee9b490facea7266375
Sebastian Velasquez Quintero <sebasb93@gmail.com>
2018-04-02 19:04:29 -0500

    #63: [FRONT] Add routes for new child components of Inventary, Transaction and Configuration

--- cois-web/src/app/app.module.ts
+++ cois-web/src/app/app.module.ts
@@ -3,12 +3,26 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule} from '@angular/router';
 import { HttpModule } from '@angular/http';
+
+//Main components
 import { ConfigurationComponent } from './configuration/configuration.component';
 import { HomeComponent } from './home/home.component';
 import { InventaryComponent } from './inventary/inventary.component';
 import { LoginComponent } from './login/login.component';
 import { TransactionComponent } from './transaction/transaction.component';
 import { appRoutes } from './app.routing';
+
+//ChildComponents
+import { AddUserComponent } from './configuration/add-user/add-user.component';
+import { EditUserComponent } from './configuration/edit-user/edit-user.component';
+import { AddItemComponent } from './inventary/add-item/add-item.component';
+import { EditItemComponent } from './inventary/edit-item/edit-item.component';
+import { ShowItemComponent } from './inventary/show-items/show-items.component';
+import { AddInputTransactionComponent } from './transaction/add-input-transaction/add-input-transaction.component';
+import { AddOutputTransactionComponent } from './transaction/add-output-transaction/add-output-transaction.component';
+import { ShowMyTransactionsComponent } from './transaction/show-my-transactions/show-my-transactions.component';
+import { ShowTransactionsComponent } from './transaction/show-transactions/show-transactions.component';
+import { AuthorizeTransactionComponent } from './transaction/authorize-transaction/authorize-transaction.component';
 
 import { AppComponent } from './app.component';
 
@@ -19,7 +33,17 @@ import { AppComponent } from './app.component';
     HomeComponent,
     InventaryComponent,
     LoginComponent,
-    TransactionComponent
+    TransactionComponent,
+    AddUserComponent,
+    EditUserComponent,
+    AddItemComponent,
+    EditItemComponent,
+    ShowItemComponent,
+    AddInputTransactionComponent,
+    AddOutputTransactionComponent,
+    ShowMyTransactionsComponent,
+    ShowTransactionsComponent,
+    AuthorizeTransactionComponent
   ],
   imports: [
     BrowserModule,
