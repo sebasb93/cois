1da79c91916cc94fb39238de654e26c2de55deb4
Sebastian Velasquez Quintero <sebasb93@gmail.com>
2018-04-02 22:43:46 -0500

    #6: Add front-end menus

--- cois-web/src/app/app.component.ts
+++ cois-web/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  providers: [NgbModule],
+  styleUrls: [
+    './app.component.css',
+    '../assets/css/bootstrap.min.css'
+  ]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'COIS-WEB';
 }
