3724bf18453b65dfec0e0d8750e1ec9819a18765
Sebastian Velasquez Quintero <sebasb93@gmail.com>
2018-04-04 19:55:37 -0500

    Refactory components structure

--- cois-web/src/app/app.routing.ts
+++ cois-web/src/app/app.routing.ts
@@ -1,23 +1,23 @@
 import { Routes } from '@angular/router';
 
 //Main components
-import { ConfigurationComponent } from './configuration/configuration.component';
-import { HomeComponent } from './home/home.component';
-import { InventaryComponent } from './inventary/inventary.component';
-import { LoginComponent } from './login/login.component';
-import { TransactionComponent } from './transaction/transaction.component';
+import { ConfigurationComponent } from './components/configuration/configuration.component';
+import { HomeComponent } from './components/home/home.component';
+import { InventaryComponent } from './components/inventary/inventary.component';
+import { LoginComponent } from './components/login/login.component';
+import { TransactionComponent } from './components/transaction/transaction.component';
 
 //ChildComponents
-import { AddUserComponent } from './configuration/add-user/add-user.component';
-import { EditUserComponent } from './configuration/edit-user/edit-user.component';
-import { AddItemComponent } from './inventary/add-item/add-item.component';
-import { EditItemComponent } from './inventary/edit-item/edit-item.component';
-import { ShowItemComponent } from './inventary/show-items/show-items.component';
-import { AddInputTransactionComponent } from './transaction/add-input-transaction/add-input-transaction.component';
-import { AddOutputTransactionComponent } from './transaction/add-output-transaction/add-output-transaction.component';
-import { ShowMyTransactionsComponent } from './transaction/show-my-transactions/show-my-transactions.component';
-import { ShowTransactionsComponent } from './transaction/show-transactions/show-transactions.component';
-import { AuthorizeTransactionComponent } from './transaction/authorize-transaction/authorize-transaction.component';
+import { AddUserComponent } from './components/configuration/add-user/add-user.component';
+import { EditUserComponent } from './components/configuration/edit-user/edit-user.component';
+import { AddItemComponent } from './components/inventary/add-item/add-item.component';
+import { EditItemComponent } from './components/inventary/edit-item/edit-item.component';
+import { ShowItemComponent } from './components/inventary/show-items/show-items.component';
+import { AddInputTransactionComponent } from './components/transaction/add-input-transaction/add-input-transaction.component';
+import { AddOutputTransactionComponent } from './components/transaction/add-output-transaction/add-output-transaction.component';
+import { ShowMyTransactionsComponent } from './components/transaction/show-my-transactions/show-my-transactions.component';
+import { ShowTransactionsComponent } from './components/transaction/show-transactions/show-transactions.component';
+import { AuthorizeTransactionComponent } from './components/transaction/authorize-transaction/authorize-transaction.component';
 
 export const appRoutes: Routes = [
     { path: '', component: HomeComponent },
