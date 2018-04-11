d652f174accf2dfded704eed5a14d4a6384ef7a3
david <juan.espinosa@newshore.es>
2018-04-10 23:20:50 -0500

    front home page

--- cois-web/src/app/components/home/home.component.ts
+++ cois-web/src/app/components/home/home.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api-service';
 import { DataService } from '../../services/data-service';
 
-
 @Component({
   selector: 'app-home-component',
   templateUrl: './home.component.html'
@@ -12,12 +11,13 @@ export class HomeComponent implements OnInit {
 
   id: string;
 
-  constructor(private apiService: ApiService, private dataService: DataService) {
+  constructor(private apiService: ApiService,
+    private dataService: DataService) {
     this.id = '';
   }
 
   ngOnInit() {
     console.log(this.apiService.getTest());
-    this.id = this.dataService.getSessionKey().userId;
+    this.id = this.dataService.getSessionKey().userId;  
   }
 }
